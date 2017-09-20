ba33728d1712dea09f5eaab4b5de118524ef7883
Christian Nunciato <cnunciato@chef.io>
2017-09-20 10:00:31 -0700

    WIP

--- components/builder-web/app/shared/channels/channels.component.ts
+++ components/builder-web/app/shared/channels/channels.component.ts
@@ -7,4 +7,27 @@ import { Component, Input } from "@angular/core";
 export class ChannelsComponent {
 
   @Input() channels: string[];
+
+  expanded: boolean = false;
+
+  stylesFor(channel) {
+    let styles: any = {};
+
+    if (!this.expanded) {
+      let i = this.channels.indexOf(channel);
+
+      styles.left = `${i * 10}px`;
+      styles["z-index"] = (-1 * i);
+    }
+    else {
+      styles.left = `0`;
+      styles["z-index"] = 1;
+    }
+
+    return styles;
+  }
+
+  toggle() {
+    this.expanded = !this.expanded;
+  }
 }
