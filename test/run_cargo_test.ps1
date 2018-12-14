f1af0cad518893daefbdd74194bcc9bb7b9ddca2
Scott Hain <shain@chef.io>
2018-12-13 16:11:04 -0800

    cmon windows

--- test/run_cargo_test.ps1
+++ test/run_cargo_test.ps1
@@ -10,12 +10,14 @@ param (
 $ErrorActionPreference="stop"
 $current_protocols = [Net.ServicePointManager]::SecurityProtocol
 try {
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    Invoke-RestMethod -usebasicparsing 'https://static.rust-lang.org/rustup/dist/i686-pc-windows-gnu/rustup-init.exe' -outfile 'rustup-init.exe'
-  }
-  finally {
-      [Net.ServicePointManager]::SecurityProtocol = $current_protocols
-  }
+  [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+  Invoke-RestMethod -usebasicparsing 'https://static.rust-lang.org/rustup/dist/i686-pc-windows-gnu/rustup-init.exe' -outfile 'rustup-init.exe'
+}
+finally {
+  [Net.ServicePointManager]::SecurityProtocol = $current_protocols
+}
+& pwd
+& dir /w
 & rustup-init.exe -y --default-toolchain stable-x86_64-pc-windows-msvc
 
 Write-Host "--- Running cargo test on $Component"
