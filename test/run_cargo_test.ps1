1dda4825bf0282323099387abcd77c48b353eaf2
Scott Hain <shain@chef.io>
2018-12-13 16:05:44 -0800

    add rust

--- test/run_cargo_test.ps1
+++ test/run_cargo_test.ps1
@@ -9,6 +9,9 @@ param (
 
 $ErrorActionPreference="stop"
 
+Invoke-Restmethod -usebasicparsing 'https://static.rust-lang.org/rustup/dist/i686-pc-windows-gnu/rustup-init.exe' -outfile 'rustup-init.exe'
+& rustup-init.exe -y --default-toolchain stable-x86_64-pc-windows-msvc
+
 Write-Host "--- Running cargo test on $Component"
 & cd components/$Component
 & cargo test --verbose
