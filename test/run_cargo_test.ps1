202c137ff0e932eb15ade5ac146ed9fa35a26bbf
Scott Hain <shain@chef.io>
2018-12-18 15:45:22 -0800

    go nuts

--- test/run_cargo_test.ps1
+++ test/run_cargo_test.ps1
@@ -9,30 +9,33 @@ param (
 
 $ErrorActionPreference="stop"
 
-$cargo = "$env:userprofile\.cargo\bin\cargo.exe"
+dir
 
-Write-Host "--- Installing Visual Studio Tools"
-& hab install core/visual-cpp-build-tools-2015
+pwd
+# $cargo = "$env:userprofile\.cargo\bin\cargo.exe"
 
-# Doing this manually for the moment as POC
-$tools_path = Invoke-Expression "hab pkg path core/visual-cpp-build-tools-2015"
-$env:VCTargetsPath="$tools_path\Program Files\MSBuild\Microsoft.Cpp\v4.0\v140"
-$env:VcInstallDir="$tools_path\Program Files\Microsoft Visual Studio 14.0\VC"
-$env:WindowsSdkDir_81="$tools_path\Windows Kits\8.1"
-$env:CLTrackerSdkPath="$tools_path\Program Files\MSBuild\14.0\bin\amd64"
-$env:CLTrackerFrameworkPath="$tools_path\Program Files\MSBuild\14.0\bin\amd64"
-$env:LinkTrackerSdkPath="$tools_path\Program Files\MSBuild\14.0\bin\amd64"
-$env:LinkTrackerFrameworkPath="$tools_path\Program Files\MSBuild\14.0\bin\amd64"
-$env:LibTrackerSdkPath="$tools_path\Program Files\MSBuild\14.0\bin\amd64"
-$env:LibTrackerFrameworkPath="$tools_path\Program Files\MSBuild\14.0\bin\amd64"
-$env:RCTrackerSdkPath="$tools_path\Program Files\MSBuild\14.0\bin\amd64"
-$env:RCTrackerFrameworkPath="$tools_path\Program Files\MSBuild\14.0\bin\amd64"
-$env:DisableRegistryUse="true"
-$env:UseEnv="true"
-$env:Path="$env:Path;C:\hab\pkgs\core\visual-cpp-build-tools-2015\14.0.25420\20181108222024\Program Files\Microsoft Visual Studio 14.0\VC\bin\amd64;C:\h b\pkgs\core\visual-cpp-build-tools-2015\14.0.25420\20181108222024\Program Files\Microsoft Visual Studio 14.0\VC\redist\x64\Microsoft.VC140.CRTaC:\hab\pkgs\core\visual-cpp-build-tools-2015\14.0.25420\20181108222024\Program Files\MSBuild\14.0\bin\amd64;C:\hab\pkgs\core\visual-cpp-build-;ools-2015\14.0.25420\20181108222024\Windows Kits\8.1\bin\x64"
+# Write-Host "--- Installing Visual Studio Tools"
+# & hab install core/visual-cpp-build-tools-2015
 
-Write-Host "--- Running cargo test on $Component"
-& cd components/$Component
-& $cargo build --verbose
+# # Doing this manually for the moment as POC
+# $tools_path = Invoke-Expression "hab pkg path core/visual-cpp-build-tools-2015"
+# $env:VCTargetsPath="$tools_path\Program Files\MSBuild\Microsoft.Cpp\v4.0\v140"
+# $env:VcInstallDir="$tools_path\Program Files\Microsoft Visual Studio 14.0\VC"
+# $env:WindowsSdkDir_81="$tools_path\Windows Kits\8.1"
+# $env:CLTrackerSdkPath="$tools_path\Program Files\MSBuild\14.0\bin\amd64"
+# $env:CLTrackerFrameworkPath="$tools_path\Program Files\MSBuild\14.0\bin\amd64"
+# $env:LinkTrackerSdkPath="$tools_path\Program Files\MSBuild\14.0\bin\amd64"
+# $env:LinkTrackerFrameworkPath="$tools_path\Program Files\MSBuild\14.0\bin\amd64"
+# $env:LibTrackerSdkPath="$tools_path\Program Files\MSBuild\14.0\bin\amd64"
+# $env:LibTrackerFrameworkPath="$tools_path\Program Files\MSBuild\14.0\bin\amd64"
+# $env:RCTrackerSdkPath="$tools_path\Program Files\MSBuild\14.0\bin\amd64"
+# $env:RCTrackerFrameworkPath="$tools_path\Program Files\MSBuild\14.0\bin\amd64"
+# $env:DisableRegistryUse="true"
+# $env:UseEnv="true"
+# $env:Path="$env:Path;C:\hab\pkgs\core\visual-cpp-build-tools-2015\14.0.25420\20181108222024\Program Files\Microsoft Visual Studio 14.0\VC\bin\amd64;C:\h b\pkgs\core\visual-cpp-build-tools-2015\14.0.25420\20181108222024\Program Files\Microsoft Visual Studio 14.0\VC\redist\x64\Microsoft.VC140.CRTaC:\hab\pkgs\core\visual-cpp-build-tools-2015\14.0.25420\20181108222024\Program Files\MSBuild\14.0\bin\amd64;C:\hab\pkgs\core\visual-cpp-build-;ools-2015\14.0.25420\20181108222024\Windows Kits\8.1\bin\x64"
 
-if ($LASTEXITCODE -ne 0) {exit $LASTEXITCODE}
+# Write-Host "--- Running cargo test on $Component"
+# & cd components/$Component
+# & $cargo build --verbose
+
+# if ($LASTEXITCODE -ne 0) {exit $LASTEXITCODE}
