99edfea6c9df7cb27dc580d3ce56350e694212fc
Scott Hain <shain@chef.io>
2019-02-14 15:47:50 -0800

    hm

--- support/ci/rustfmt.sh
+++ support/ci/rustfmt.sh
@@ -2,15 +2,14 @@
 
 set -euox pipefail
 
-export RUSTUP_HOME="/opt/rust"
-export CARGO_HOME="/home/buildkite-agent/.cargo"
-export PATH="/opt/rust/bin:$PATH"
+# TODO: fix this upstream so it's already on the path and set up
+export RUSTUP_HOME=/opt/rust
+export CARGO_HOME=/home/buildkite-agent/.cargo
+export PATH=/opt/rust/bin:$PATH
+# TODO: fix this upstream, it looks like it's not saving correctly.
+sudo chown -R buildkite-agent /home/buildkite-agent
 
-ls -la
-
-which cargo
-
-/opt/rust/bin/cargo --version
+rustup component add rustfmt
 
 cargo_fmt="cargo fmt --all -- --check"
 echo "--- Running cargo fmt command: $cargo_fmt"
