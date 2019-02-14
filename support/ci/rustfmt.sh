923edfd839f0a6a70318dd0d81212a2cc43dffc9
Scott Hain <shain@chef.io>
2019-02-14 15:45:01 -0800

    what the heck

--- support/ci/rustfmt.sh
+++ support/ci/rustfmt.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-set -euo pipefail
+set -euox pipefail
 
 export RUSTUP_HOME="/opt/rust"
 export CARGO_HOME="/home/buildkite-agent/.cargo"
 export PATH="/opt/rust/bin:$PATH"
+
+ls -la
+
+which cargo
+
+/opt/rust/bin/cargo --version
 
 cargo_fmt="cargo fmt --all -- --check"
 echo "--- Running cargo fmt command: $cargo_fmt"
