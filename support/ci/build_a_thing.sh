c956c2ee36431b8b7a60f6e0b1545a31f4b79cc0
Scott Hain <shain@chef.io>
2019-02-15 10:34:34 -0800

    actually run the builds

--- support/ci/build_a_thing.sh
+++ support/ci/build_a_thing.sh
@@ -3,15 +3,9 @@
 set -xeou pipefail
 
 component=${1?component argument required}
-# cargo_test_command="cargo test ${features_string} -- --nocapture ${test_options:-}"
 
-# # TODO: fix this upstream so it's already on the path and set up
-# export RUSTUP_HOME=/opt/rust
-# export CARGO_HOME=/home/buildkite-agent/.cargo
-# export PATH=/opt/rust/bin:$PATH
-# # TODO: fix this upstream, it looks like it's not saving correctly.
-# sudo chown -R buildkite-agent /home/buildkite-agent
 export HAB_ORIGIN
 HAB_ORIGIN=throwaway
+# is this going to cause a problem if we have multiple agents on the same machine?
 hab origin key generate
 hab pkg build -D components/$component
