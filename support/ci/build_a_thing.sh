a666ba18162e44fd2da4cb6df12e8f0aac46bee4
Scott Hain <shain@chef.io>
2019-02-15 10:51:18 -0800

    move studios off even more

--- support/ci/build_a_thing.sh
+++ support/ci/build_a_thing.sh
@@ -4,9 +4,17 @@ set -xeou pipefail
 
 component=${1?component argument required}
 
+# Since we are only verifying we don't have build failures, make everything
+# temp!
 export HAB_ORIGIN
 HAB_ORIGIN=throwaway
+# let's make a selfcontained tempdir for this job
+export JOB_TEMP_ROOT
+JOB_TEMP_ROOT=$(mktemp -d /tmp/job-root-XXXXXX)
 export HAB_CACHE_KEY_PATH
-HAB_CACHE_KEY_PATH=$(mktemp -d /tmp/throwaway-keys-XXXXXX)
+HAB_CACHE_KEY_PATH="$JOB_TEMP_ROOT/keys"
+export HAB_STUDIOS_HOME
+HAB_STUDIOS_HOME="$JOB_TEMP_ROOT/studios"
+
 hab origin key generate
 hab pkg build -D components/$component
