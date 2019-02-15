e31ffe52b6ce80fd5315cc84d9fa1e75ee5f7993
Scott Hain <shain@chef.io>
2019-02-15 10:46:06 -0800

    make temp key

--- support/ci/build_a_thing.sh
+++ support/ci/build_a_thing.sh
@@ -6,6 +6,7 @@ component=${1?component argument required}
 
 export HAB_ORIGIN
 HAB_ORIGIN=throwaway
-# is this going to cause a problem if we have multiple agents on the same machine?
+export HAB_CACHE_KEY_PATH
+HAB_CACHE_KEY_PATH=$(mktemp -d /tmp/throwaway-keys-XXXXXX)
 hab origin key generate
 hab pkg build -D components/$component
