0e9daf67b0268097fd4485f3f96a71bbd4f3293d
Scott Hain <shain@chef.io>
2019-02-15 11:54:11 -0800

    flooby

--- support/ci/build_a_thing.sh
+++ support/ci/build_a_thing.sh
@@ -9,17 +9,12 @@ component=${1?component argument required}
 export HAB_ORIGIN
 HAB_ORIGIN=throwaway
 # let's make a selfcontained tempdir for this job
-# export JOB_TEMP_ROOT
-# JOB_TEMP_ROOT=$(mktemp -d /tmp/job-root-XXXXXX)
-# export HAB_CACHE_KEY_PATH
-# HAB_CACHE_KEY_PATH="$JOB_TEMP_ROOT/keys"
-# export HAB_STUDIOS_HOME
-# HAB_STUDIOS_HOME="$JOB_TEMP_ROOT/studios"
-# use undocumented variables!
-export FS_ROOT
-FS_ROOT="$(mktemp -d /tmp/job-root-XXXXXX)"
+export JOB_TEMP_ROOT
+JOB_TEMP_ROOT=$(mktemp -d /tmp/job-root-XXXXXX)
+export HAB_CACHE_KEY_PATH
+HAB_CACHE_KEY_PATH="$JOB_TEMP_ROOT/keys"
+export HAB_STUDIOS_HOME
+HAB_STUDIOS_HOME="$JOB_TEMP_ROOT/studios"
 
 hab origin key generate
-hab pkg build components/$component
-
-ls -la "$FS_ROOT"
+hab pkg build -D components/$component
