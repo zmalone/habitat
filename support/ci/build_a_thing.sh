61edc60349b827567fcd6eb7bb64a46a3e4f4bf6
Scott Hain <shain@chef.io>
2019-02-15 09:38:46 -0800

    huh.

--- support/ci/build_a_thing.sh
+++ support/ci/build_a_thing.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-set -eou pipefail
+set -xeou pipefail
 
 component=${1?component argument required}
 # cargo_test_command="cargo test ${features_string} -- --nocapture ${test_options:-}"
+
+whoami
 
 # # TODO: fix this upstream so it's already on the path and set up
 # export RUSTUP_HOME=/opt/rust
